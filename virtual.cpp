9a8325e00ed87091f9ee870c1b815d80d8b451f7
sumit kushwah <sumitkushwah962@gmail.com>
2025-05-04 13:58:47 +0530

    this is a commit

---
+++ virtual.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class base
+{
+public:
+void fun()
+{
+cout<<"this is a base class"<<endl;
+}
+};
+
+class derive:public base
+{
+public:
+void fun()
+{
+cout<<"this is a derive class"<<endl;
+}
+};
+int main()
+{
+base *p=new derive;
+p->fun();
+return(0);
+}
